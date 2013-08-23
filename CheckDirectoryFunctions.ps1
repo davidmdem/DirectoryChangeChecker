d4f595e5121e774c36176d0236944b6bc6aab42b
dmdemeul <dmdemeul@indiana.edu>
2013-08-23 11:41:40 -0400

    Moved functions to a dot sourced file.  Added input checking.  Added handling empty checkPath.

---
+++ CheckDirectoryFunctions.ps1
@@ -0,0 +1,33 @@
+﻿# Slightly modified from @davor's comment in 
+# http://stackoverflow.com/questions/10521061/how-to-get-a-md5-checksum-in-powershell
+function md5($file) {
+	$md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+	[System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::Open("$file",[System.IO.Filemode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::Read)))
+}
+
+# Removes trailing slash from a string
+function chopSlash($string) {
+	if($string.substring($string.length - 1, 1) -eq "\") {
+		return $string -replace ".$"
+	} else {
+		return $string
+	}
+}
+
+function addSlash($string) {
+	if($string.substring($string.length - 1, 1) -eq "\") {
+		return $string
+	} else {	
+		return $string + "\"
+	}
+}
+
+# Puts output to a file and exits.
+function outputResult($result) {
+	if($resultFilePath -eq $null) {
+		echo $result
+	} else {
+		$result | Out-File $resultFilePath	
+	}	
+	Exit
+}
